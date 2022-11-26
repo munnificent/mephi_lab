0ad0c6534a262d575fede04c00a2f96dbf15d25d
munnificent <82195316+munnificent@users.noreply.github.com>
2022-11-26 08:32:43 +0200

    `232

--- Test/test3.c
+++ Test/test3.c
@@ -1,12 +1,11 @@
 #include<stdio.h.>
-int bit(int a){
-    int q=0, fin=0;
-    while(a!=0){
-        q=a%10;
-        fin+=q;
-        a/=10;
-    }
-    return fin;
+void bit(int arr[], int b){
+        //ввод в мссив элементов //проход строка
+        int i=1;
+        for (int j = 0; j < b; j++){ //проход столбец
+            printf("[%d][%d]:",i,j);
+             scanf("%d", &arr[j]);
+            }
 }
 int main(){
         int a , b, c, d, flag ; //кол-во строк и столбцов
@@ -14,32 +13,13 @@ int main(){
     scanf("%d",&a);
     printf("Vedite chislo stolbcov:");
     scanf("%d",&b);
-    a=2;
-    b=5;
 
-    int arr[2][5];
+    int arr[b];
+    bit(arr,b);
 
-    //ввод в мссив элементов
-    for (int i = 0; i < a; i++){ //проход строка
-        for (int j = 0; j < b; j++){ //проход столбец
-            printf("[%d][%d]:",i,j);
-            scanf("%d", &arr[i][j]);
-            }
-    }
-
-   arr[1][1]=arr[1][2];
-
-
-
-    for (int i = 0; i <= a; i++){ //проход строк
-        printf("\n| ");
+        printf("\n ");
         for (int j = 0; j < b; j++){ //проход столбов
-            printf("%d ", arr[i][j]);
-            if(j == b-1){
-                printf(" |");
+            printf("%d ", arr[j]);
             }
-        }
-
-    }
     return 0;
 }
